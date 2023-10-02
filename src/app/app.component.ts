c68f31f8771c3fc71e3968036bee47a003986e25
santhoshkumar <its.santhoshkumar@outlook.com>
2023-10-02 16:46:58 +0530

    create hard coded address block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamic-form-group';
+  personForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+  ) {
+
+  }
+  ngOnInit() {
+    // console.log('this is called')
+    this.personForm = this.fb.group({
+      name: new FormControl(''),
+      deliveryAddress: this.fb.group({
+        address: new FormControl(''),
+        pincode: new FormControl('')
+      })
+    })
+  }
+
+  onSubmit() {
+    console.log(this.personForm.value)
+  }
 }
